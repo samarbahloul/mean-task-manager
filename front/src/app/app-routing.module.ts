a3f29f82c2a4e50f0cd50ee186b8f3055f53a89c
samarbahloul <88493580+samarbahloul@users.noreply.github.com>
2022-04-20 01:52:19 +0100

    Add files via upload

---
+++ front/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NewListComponent } from './pages/new-list/new-list.component';
+import { TaskViewComponent } from './pages/task-view/task-view.component';
+
+const routes: Routes = [
+  { path: '', component: TaskViewComponent },
+  { path: 'new-list', component: NewListComponent },
+  { path: 'lists/:listId', component: TaskViewComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
